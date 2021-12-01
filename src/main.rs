18f644a60a853f4c4cd37c391a95e522d78fc605
Austin Burke <austinlayneburke@gmail.com>
2021-12-01 06:27:34 -0600

    Adding test fn and notes

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,28 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
-fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
+
+fn main() {
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
+    // instead of:
+    //      hello(&(*m)[..]);
+    // The (*m) dereferences the MyBox<String> into a String. 
+    // Then the & and [..] take a string slice of the String 
+    //   that is equal to the whole string to match the signature of hello
+}
+
+// fn main() {
+//     let x = 5;
+//     let y = MyBox::new(x);
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
 
 // fn main() {
 //     let x = 5;
@@ -31,6 +46,10 @@ fn main() {
 //     assert_eq!(5, x);
 //     assert_eq!(5, *y);
 // }
+
+
+
+// ERRORS
 
 // fn main() {
 //     let x = 5;
