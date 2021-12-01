8ddcb185c29a16e835fd5d5195ed2c096fb232dd
Austin Burke <austinlayneburke@gmail.com>
2021-12-01 05:49:41 -0600

    Re-implementing Box

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+// fn main() {
+//     let x = 5;
+//     let y = Box::new(x);
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
+
+// fn main() {
+//     let x = 5;
+//     let y = &x;
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, y);
+// }
+
